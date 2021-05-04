7eb9d25e2c66c43e52a72df89292bb1ee9e90f8a
Lisa Mittmann <li.mittmann@gmail.com>
2021-05-04 22:18:32 +0200

    :tada: Initialize spring

---
+++ src/main/java/de/otto/beluga/amazonipranges/AmazoniprangesApplication.java
@@ -0,0 +1,13 @@
+package de.otto.beluga.amazonipranges;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazoniprangesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazoniprangesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/otto/beluga/amazonipranges/AmazoniprangesApplicationTests.java
@@ -0,0 +1,13 @@
+package de.otto.beluga.amazonipranges;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazoniprangesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
